6ac753482d11dca4e5ed8f4edf563fd78ed6bc1a
kuro11pow2 <kuro11pow2@gmail.com>
2022-01-02 00:59:22 +0900

    저장 데이터 구조 변경

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ today = str(datetime.now(timezone(timedelta(hours=0))).date())
 target_dir = os.path.join(os.getcwd(), 'data')
 target_full = os.path.join(target_dir, 'youtube.json')
 channels = {
-    '5-Minute Crafts':'c/5MinuteCraftsYouTube',
-    '침착맨':'channel/UCUj6rrhMTR9pipbAWBAMvUQ'
+    'c/5MinuteCraftsYouTube':'5-Minute Crafts',
+    'channel/UCUj6rrhMTR9pipbAWBAMvUQ':'침착맨'
     }
-
 data = None
 
 if not os.path.exists(target_dir):
     print('기존 디렉터리가 존재하지 않음.')
     os.makedirs(target_dir)
+
 try:
     with open(target_full, 'r', encoding="UTF-8-sig") as f:
         data = json.JSONDecoder().decode(f.read())
@@ -23,14 +23,16 @@ except Exception as e:
     print('기존 파일이 존재하지 않음.')
     data = dict()
 
-for name, id in channels.items():
+for id, name in channels.items():
+
     print(f'{name} 조회')
     cnt = ChannelAbout(id).request().parse_view()
 
-    if name not in data.keys(): 
-        data[name] = dict()
-    if today not in data[name].keys():
-        data[name] = {**data[name], **{today: cnt}}
+    if id not in data.keys(): 
+        data[id] = {'name':name, 'viewCounts':dict()}
+
+    if today not in data[id]['viewCounts'].keys():
+        data[id]['viewCounts'] = {**data[id]['viewCounts'], **{today: cnt}}
         print(data)
     else:
         print(f'{today} 데이터 존재함: {name}, {cnt}')
--- script.js
+++ script.js
@@ -3,73 +3,26 @@
 
     window.onload = function () {
 
-        function getRand(start, size) {
-            return Math.floor(Math.random() * size) + start;
-        }
-
-        function getRandomDataset(bot, top, size) {
-            dataset = getDataset(test, []);
-
-            for (let i = 0; i < size; i++)
-                dataset.data.push(getRand(bot, top));
-
-            return dataset;
+        function getRandClosed(start, end) {
+            return Math.floor(Math.random() * (end - start + 1)) + start;
         }
 
         function getDataset(name, dataArr) {
             dataset = {
                 label: name,
-                borderColor: 'rgb(' + [getRand(50, 200), getRand(50, 200), getRand(50, 200)].join(',') + ')',
+                borderColor: 'rgb(' + [getRandClosed(0, 200), getRandClosed(0, 200), getRandClosed(0, 200)].join(',') + ')',
                 fill: false,
                 data: dataArr
             }
             return dataset
         }
 
-        function getData(jsondata) {
-
-            let data = {
-                labels: [],
-                datasets: []
-            };
-
-            let allDates = new Set();
-
-            for (const name in jsondata) {
-                for (var date of Object.keys(jsondata[name])) {
-                    allDates.add(date);
-                }
+        class ChannelAbout {
+            constructor(id, name, viewCountsObj = null) {
+                this.id = id;
+                this.name = name;
+                this.viewCounts = viewCountsObj ? new Map(Object.entries(viewCountsObj)) : new Map();
             }
-
-            data.labels = Array.from(allDates).slice(1);
-
-            for (const name in jsondata) {
-
-                let curDates = new Set(Object.keys(jsondata[name]));
-                let dataArr = [];
-
-                allDates.forEach(date => {
-
-                    if (curDates.has(date)) {
-                        dataArr.push(jsondata[name][date]);
-                    }
-                    else {
-                        if (dataArr.length > 0) {
-                            dataArr.push(dataArr[dataArr.length - 1])
-                        }
-                    }
-
-                })
-
-                for (let i = dataArr.length - 1; i > 0; i--) {
-                    dataArr[i] = Math.log10(dataArr[i] - dataArr[i - 1]);
-                }
-
-                let dataset = getDataset(name, dataArr.slice(1));
-                data.datasets.push(dataset);
-            }
-
-            return data;
         }
 
 
@@ -80,9 +33,69 @@
                 .then(response => {
                     return response.json();
                 })
-                .then(jsondata => {
-                    data = getData(jsondata);
+                .then(channelAboutArrJson => {
 
+                    let channelAboutArr = new Array();
+
+                    for (const id in channelAboutArrJson) {
+                        let channelAboutJson = channelAboutArrJson[id];
+                        let name = channelAboutJson['name'];
+                        let viewCountsObj = channelAboutJson['viewCounts'];
+                        channelAboutArr.push(new ChannelAbout(id, name, viewCountsObj))
+                    }
+                    console.log(channelAboutArr);
+
+                    // 출력할 모든 날짜 집합 구하기
+                    let allDateSet = new Set();
+
+                    for (let channelAbout of channelAboutArr) {
+                        let dates = channelAbout.viewCounts.keys();
+
+                        for (let date of dates) {
+                            allDateSet.add(date);
+                        }
+                    }
+                    console.log(allDateSet);
+
+                    // 각 채널에 대하여 출력할 데이터를 계산
+                    let viewCountsDiffArrMap = new Map();
+                    for (let ch of channelAboutArr) {
+
+                        // 출력해야 하는 날인데 데이터가 누락된 경우 전날의 데이터를 사용하도록 함
+                        let viewCountArr = new Array();
+                        allDateSet.forEach(date => {
+                            if (ch.viewCounts.has(date)) {
+                                viewCountArr.push(ch.viewCounts.get(date));
+                            }
+                            else if (viewCountArr.length > 0) {
+                                viewCountArr.push(viewCountArr[viewCountArr.length - 1]);
+                            }
+                        })
+
+                        // 날짜 간 조회수 차이에 상용 로그를 담은 배열을 생성 (크기가 -1 됨)
+                        let viewCountDiffArr = new Array();
+                        for (let i = 0; i < viewCountArr.length - 1; i++) {
+                            viewCountDiffArr.push(Math.log10(viewCountArr[i + 1] - viewCountArr[i]));
+                        }
+
+                        viewCountsDiffArrMap.set(ch.id, viewCountDiffArr);
+                    }
+                    console.log(viewCountsDiffArrMap);
+
+                    // 차트에 전달할 형태로 데이터 변환
+                    let data = {
+                        labels: Array.from(allDateSet).slice(1),
+                        datasets: new Array()
+                    };
+
+                    for (let ch of channelAboutArr) {
+                        let name = ch.name;
+                        let dataArr = viewCountsDiffArrMap.get(ch.id);
+                        let dataset = getDataset(name, dataArr);
+                        data.datasets.push(dataset);
+                    }
+
+                    // 차트 생성
                     const options = {
                         maintainAspectRatio: false,
                     };

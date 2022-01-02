74e6ecfb5009144dc6151325a3d5041682c89e2f
kuro11pow2 <kuro11pow2@gmail.com>
2022-01-02 16:33:26 +0900

    채널 추가

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ target_dir = os.path.join(os.getcwd(), 'data')
 target_full = os.path.join(target_dir, 'youtube.json')
 channels = {
     'c/5MinuteCraftsYouTube':'5-Minute Crafts',
-    'channel/UCUj6rrhMTR9pipbAWBAMvUQ':'침착맨'
+    'channel/UCUj6rrhMTR9pipbAWBAMvUQ':'침착맨',
+    'channel/UC3WZlO2Zl8NE1yIUgtwUtQw':'임영웅',
+    'channel/UC3SyT4_WLHzN7JmHQwKQZww':'이지금 [IU Official]',
+    'channel/UCOp66Vup07X0YziXaaxqs2A':'haha ha',
+    'channel/UCzgNzUJkXaUbVgHyiycQ2Zw':'휘용 Hwiyong',
+    'channel/UCLKuglhGlMmDteQKoniENIQ':'14F 일사에프',
+    'c/BANGTANTV':'BANGTANTV',
+    'c/JFlaMusic':'JFlaMusic',
+    'user/woowakgood':'우왁굳의 게임방송',
+    'channel/UCV9WL7sW6_KjanYkUUaIDfQ':'고세구 GOSEGU'
     }
 data = None
 
